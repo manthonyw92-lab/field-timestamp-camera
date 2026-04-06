c617eecba1193bcd37317b9765f666e566ee346c
Mikel Wentworth <mwentworth@freedomforever.com>
2026-04-06 16:54:24 -0500

    Fix: disable R8 minification to prevent Flutter class stripping

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -33,12 +33,8 @@ android {
     buildTypes {
         release {
             signingConfig = signingConfigs.getByName("debug")
-            isMinifyEnabled = true
-            isShrinkResources = true
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
+            isMinifyEnabled = false
+            isShrinkResources = false
         }
     }
 }
